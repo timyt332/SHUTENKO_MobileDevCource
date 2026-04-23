6619c006ca5466a51393c3d3def95295c281d38a
timyt332 <timutwertufoon@gmail.com>
2026-04-23 16:33:38 +0300

    pr6_1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,12 +2,7 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: true,
-        tabBarActiveTintColor: "#007AFF",
-      }}
-    >
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: "Головна" }} />
       <Tabs.Screen name="sandbox" options={{ title: "Пісочниця" }} />
       <Tabs.Screen name="profile" options={{ title: "Профіль" }} />
